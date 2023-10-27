6ca79c7d8bf55eda601e6b899343fdcf1861eae7
Carlos Precioso <github@precioso.design>
2023-10-27 10:24:45 +0200

    feat: update api endpoint

--- src/fetch-parkings.mjs
+++ src/fetch-parkings.mjs
@@ -3,25 +3,29 @@
 import got from "got";
 import assert from "node:assert/strict";
 
+const DATASET_URL =
+  "https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/aparcaments-bicicletes-aparcamientos-bicicletas";
 const GEOJSON_URL =
-  "https://geoportal.valencia.es/apps/OpenData/Trafico/tra_bici_aparcamiento.json";
+  "https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/aparcaments-bicicletes-aparcamientos-bicicletas/exports/geojson";
+
+export const getModifiedDateString = async () => {
+  const data = await got(DATASET_URL).json();
+
+  const modified = data?.metas?.default?.modified;
+  if (modified) return modified;
+};
 
 export const fetchParkings = async () => {
-  const dataResponse = await got(GEOJSON_URL);
+  const dataResponse = await got(GEOJSON_URL, { responseType: "json" });
 
-  const lastModifiedHeader = dataResponse.headers["last-modified"];
-  const updatedAt = lastModifiedHeader
-    ? new Date(lastModifiedHeader)
-    : new Date(0);
+  const updatedAtString =
+    dataResponse.headers["last-modified"] || (await getModifiedDateString());
+  const updatedAt = updatedAtString ? new Date(updatedAtString) : new Date(0);
 
   /** @type {import("../types/geojson-response").GeoJSONResponse} */
-  const data = JSON.parse(dataResponse.body);
+  const data = dataResponse.body;
 
   assert.equal(data.type, "FeatureCollection");
-  assert(
-    data.name === "gis.TRA_BICI_APARCAMIENTO" ||
-      data.name === "TRA_BICI_APARCAMIENTO"
-  );
 
   const spots = data.features.map((feat) => {
     assert(feat.type, "feature");
